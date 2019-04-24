051b4c9f34f52055de336a23a9f40fb677af0b41
lsaenz <lsaenz@moorea.io>
2019-04-24 16:30:07 -0300

    .gitignore

--- node_modules/ng-zorro-antd/core/tree/nz-tree-service.resolver.d.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { NzTreeBaseService } from './nz-tree-base.service';
-export declare const NzTreeHigherOrderServiceToken: InjectionToken<NzTreeBaseService>;
